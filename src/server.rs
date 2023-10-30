2d626fba10dd60dc60f3fce55aa228fe6d9db08f
Majjcom <majjcom@126.com>
2023-10-30 13:06:28 +0000

    up

--- src/server.rs
+++ src/server.rs
@@ -1,20 +1,35 @@
 use crate::conn::*;
+use rayon::{ThreadPool, ThreadPoolBuilder};
+use std::io::{Read, Write};
 use std::net::TcpListener;
-
-mod pool;
 
 pub struct Server {
     tcp: TcpListener,
     matcher: Option<&'static FnMatcher>,
+    pool: ThreadPool,
 }
 
 impl Server {
     fn new(addr: &str) -> Server {
         let tcp = TcpListener::bind(addr).unwrap();
-        Server { tcp, matcher: None }
+        let pool = ThreadPoolBuilder::new().num_threads(16).build().unwrap();
+        Server {
+            tcp,
+            matcher: None,
+            pool,
+        }
     }
 
-    fn start(&mut self) {}
+    fn start(&mut self) {
+        for stream in self.tcp.incoming() {
+            match stream {
+                Ok(s) => self.pool.spawn(move || loop {
+                    let mut buff: [u8; 8];
+                }),
+                Err(_) => (),
+            }
+        }
+    }
 }
 
 impl RConnection for Server {
