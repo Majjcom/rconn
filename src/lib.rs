78b170451b2ea1074f2ac280a059e4c774e1b220
Majjcom <majjcom@126.com>
2023-10-30 13:32:14 +0000

    up

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,12 @@ pub mod server;
 pub mod conn;
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::server;
+
+    #[test]
+    fn run_server() {
+        let mut s = server::Server::new("0.0.0.0:5000");
+        s.start();
+    }
+}
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
 use crate::conn::*;
 use rayon::{ThreadPool, ThreadPoolBuilder};
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use serde_json::{de, Value};
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
 
 pub struct Server {
     tcp: TcpListener,
@@ -9,8 +10,35 @@ pub struct Server {
     pool: ThreadPool,
 }
 
+fn get_stream_header_size(s: &mut TcpStream) -> usize {
+    let mut buff = Vec::new();
+    buff.resize(8, 0u8);
+    let mut size = 0;
+    while size != 8 {
+        let readed_size = s.read(&mut buff[size..]).unwrap();
+        size += &readed_size;
+    }
+    let mut size: usize = 0;
+    for i in 0..8 {
+        size += buff[i] as usize;
+        size <<= 1;
+    }
+    size
+}
+
+fn get_header_json(s: &mut TcpStream, header_size: usize) -> Value {
+    let mut header_buffer = Vec::new();
+    header_buffer.resize(header_size, 0u8);
+    let mut size = 0;
+    while size != header_size {
+        let readed_size = s.read(&mut header_buffer[size..]).unwrap();
+        size += &readed_size;
+    }
+    de::from_slice(&header_buffer).unwrap()
+}
+
 impl Server {
-    fn new(addr: &str) -> Server {
+    pub fn new(addr: &str) -> Server {
         let tcp = TcpListener::bind(addr).unwrap();
         let pool = ThreadPoolBuilder::new().num_threads(16).build().unwrap();
         Server {
@@ -20,11 +48,14 @@ impl Server {
         }
     }
 
-    fn start(&mut self) {
+    pub fn start(&mut self) {
         for stream in self.tcp.incoming() {
             match stream {
-                Ok(s) => self.pool.spawn(move || loop {
-                    let mut buff: [u8; 8];
+                Ok(mut s) => self.pool.spawn(move || loop {
+                    // Read Start
+                    let header_size = get_stream_header_size(&mut s);
+                    let header_data = get_header_json(&mut s, header_size);
+                    println!("Data: {:?}", header_data);
                 }),
                 Err(_) => (),
             }
